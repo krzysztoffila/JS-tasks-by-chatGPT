65971721c898a360dcb9075f2019793b99011336
krzysztoffila <umowy.fila@gmail.com>
2024-02-15 14:41:58 +0100

    5. Short Circuiting

--- shortCircuiting.js
+++ shortCircuiting.js
@@ -6,3 +6,12 @@ books[0].title || console.log('No title');
 function bookLanguage(book) {
   return (book.language ||= 'english');
 }
+// 23
+books[1].onlineContent && console.log(`Second book provided online content`);
+// 24
+function howManyPages(book) {
+  return (book.pages ||= 'unknown');
+}
+// 25
+const [, secondBook] = books;
+secondBook.keywords && console.log(secondBook.keywords[0]);
