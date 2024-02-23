ca24162c8fdeef27683114f6b1f044b7a5369c4b
krzysztoffila <umowy.fila@gmail.com>
2024-02-23 09:29:47 +0200

    13. Maps: Fundamentals

---
+++ mapsFundamentals.js
@@ -0,0 +1,14 @@
+'use strict';
+import books from './books.js';
+// 59
+const authorsMap = new Map();
+books.forEach((book, index) => {
+  authorsMap.set(index, book.author[0]);
+});
+console.log(authorsMap);
+// 60
+const getAllAuthors = (authorsMap) => {
+  const allAuthors = [...authorsMap.values()];
+  return allAuthors;
+};
+console.log(getAllAuthors(authorsMap));
